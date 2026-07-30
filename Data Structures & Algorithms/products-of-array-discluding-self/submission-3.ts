d795dae74562ed47089974c4207a469dbe7d3b49
Sudwipto Kumar Mondal <94325115+sudwiptokm@users.noreply.github.com>
2026-07-30 19:32:29 +0100

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        let left= [], right = [], res = []
+        const s = nums.length
+
+        right[s-1] = 1
+        left[0]=1
+
+        let val = nums[0]
+        for (let i=1; i<s; i++){
+            left[i] = val
+            val = val*nums[i]
+        }
+
+        val = nums[s-1]
+        for (let i=s-2;i>=0;i--){
+            right[i] = val
+            val *= nums[i]
+        }
+        for (let i=0; i<s;i++){
+            res[i] = left[i]*right[i] 
+        }
+
+        return res
+    }
+}
